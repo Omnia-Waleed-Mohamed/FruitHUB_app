7a070ed4dfe056580c9f82175d7087c435bb3804
Omnia-Waleed-Mohamed <omniaw03@gmail.com>
2025-10-10 17:18:48 +0300

    home view

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/core/const/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,12 +7,158 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            "Welcome Back!",
-            style: TextStyle(fontSize: 13, height: 1.6, color: Colors.black54),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Column(
+          children: [
+            Text(
+              "Welcome Back !",
+              style: TextStyle(
+                fontSize: 18,
+                height: 1.6,
+                color: Colors.black54,
+              ),
+            ),
+            Text(
+              "Omnia Waleed ",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: AppColor.kPrimaryColor,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+        
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.black),
+            onPressed: () {},
           ),
+          IconButton(
+            icon: const Icon(Icons.notifications_none, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+          const SizedBox(height: 20),
+          Image.asset("assets/images/home.png"),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                const Text("Recommendation "),
+                const Spacer(),
+                Text(
+                  "See All",
+                  style: TextStyle(color: AppColor.kPrimaryColor),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+
+           Padding(
+             padding: const EdgeInsets.only(left:10 ),
+             child: Container(
+        
+                width: 200,
+                height: 280,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 6,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      
+                      Stack(
+                        alignment: Alignment.topRight,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              "assets/images/home1.png",
+                              width: double.infinity,
+                              height: 180,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.favorite_border),
+                            color: Colors.red,
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+             
+                      const SizedBox(height: 10),
+             
+                      
+                      const Text(
+                        "Strawberry",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+             
+                      const SizedBox(height: 6),
+             
+                     
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "\$350.00",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.green,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              
+                            },
+                            borderRadius: BorderRadius.circular(50),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: AppColor.kPrimaryColor,
+                                shape: BoxShape.circle,
+                              ),
+                              padding: const EdgeInsets.all(6),
+                              child: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+           ),
+          
+          
         ],
       ),
     );
